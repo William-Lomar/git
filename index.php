97a3507fea6244878ffc77dd8782adc1462ee3ae
William <lomarw99@gmail.com>
2021-02-04 13:52:05 -0300

    meu primeiro commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GIT</title>
+</head>
+<body><!-- detalhe: todos os nomes entre aspas duplas -->
+	<p>Comando: git init -> iniciar repositório</p>
+	<p>Comando: git config --global user.name "(Seu nome)"</p>
+	<p>Comando: git add -> adiciona arquivo para ser monitorado</p>
+	<p>Comando: git add -A -> adiciona todos os arquivo no repositório</p>
+	<p>Comando: git diff-> Mostra mais detalhes doq foi alterado</p>
+	<p>Comando: git commit -m "Nome do repositorio"-> Envia o commit (tem q dar um add antes)</p>
+</body>
+</html>
