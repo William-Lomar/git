c6764906a8ea1ce321ff29faf53bd6eac34e4e77
William <lomarw99@gmail.com>
2021-02-04 14:46:28 -0300

    meu commit externo

--- index.php
+++ index.php
@@ -9,6 +9,17 @@
 	<p>Comando: git add -> adiciona arquivo para ser monitorado</p>
 	<p>Comando: git add -A -> adiciona todos os arquivo no repositório</p>
 	<p>Comando: git diff-> Mostra mais detalhes doq foi alterado</p>
-	<p>Comando: git commit -m "Nome do repositorio"-> Envia o commit (tem q dar um add antes)</p>
+	<p>Comando: git commit -m "Nome do commit"-> Envia o commit (tem q dar um add antes)</p>
+	<p>Comando: git log -> Mostra relatório de commit</p>
+	<p>Comando: git branch -> "Area de trabalho"</p>
+	<p>Comando: git checkout "Nome" -> muda a area de trabalho atual</p>
+	<p>Comando: git add -A && git commit -m "Nome do commit"</p>
+	<p>Comando: git reset --hard (Número do commit encontrado no log)</p>
+	<p>Comando: git remote -v -> verifica o repositório remoto</p>
+	<p>Comando: git remote rm origin -> remove diretório externo da configuração</p>
+	<p>Comando: git remote add origin (url) -> adiciona endereço externo do repositório</p>
+	<p>Comando: git pull origin master -> baixa o repositório externo</p>
+	<p>Comando: git push origin master -> envia o repositório</p>
+
 </body>
 </html>
